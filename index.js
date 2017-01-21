81f0bfe4a698f51d7d368346baa543a8bda19fbe
Blake Worsley <blakeworsley@gmail.com>
2017-01-21 12:42:23 -0700

    adds multiple summaries and renders by day

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var $calendar = $('.calendar');
-var cal_id = '';
+var cal_id = 'okoi8uh03p6ik4bsc0lfa6vgs8@group.calendar.google.com';
 var api_key = '';
 var params = {
   key: api_key,
@@ -24,26 +24,42 @@ function getCalendar(pageToken) {
 }
 
 function displayEvents(data) {
+  var days = {Monday:[], Tuesday:[], Wednesday:[], Thursday:[], Friday:[], Saturday:[], Sunday:[]};
   data.map( function (item) {
     var date = moment(item.start.dateTime);
-    var day = date.format('D');
     var dayOfWeek = date.format('dddd');
-    var summary = function(){
-      item.summary
-    }
-    $calendar.append(calendarDayHtml(day, item.summary, dayOfWeek, 'food truck'));
+    days[dayOfWeek].push(item)
   });
+
+  var keys = Object.keys(days)
+  for(var i = 0; i < keys.length; i++){
+    var eachDay = days[keys[i]];
+    var stubbedDay = calendarDayHtml(eachDay, keys[i]);
+    $calendar.append(stubbedDay);
+  }
 }
 
-function calendarDayHtml(date, summary, day, photo) {
+function calendarDayHtml(day, dayOfWeek) {
+  var date = 00;
+  var summaries = ''
+  if(day.length){
+    date = moment(day[0].start.dateTime).format('D');
+    for(var i = 0; i < day.length; i++){
+      summaries += 
+        `<p>${day[i].summary + ' ' + 
+        moment(day[i].start.dateTime).format('LT') + 
+        '-' + moment(day[i].end.dateTime).format('LT') 
+        }</p>`;
+    }
+  }
   return ` 
     <section class="calendar-individual-day">
       <section class="individual-day-date">
         <h2 class="date-numeral">${date}</h2>
-        <h1 class="date-text">${day}</h1>
+        <h1 class="date-text">${dayOfWeek}</h1>
       </section>
       <section class="individual-day-activities">
-        <p>${summary}</p>
+        ${summaries}
       </section>
       <section class="individual-day-photos">
         <img src="truck.svg"/>
