e4eb9d599717abd07423e30cfe4191682d445008
Blake Worsley <blakeworsley@gmail.com>
2017-02-02 22:46:20 -0700

    adds food truck rendering based on matching FT:

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: {
@@ -14,7 +16,27 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
     ]
   },
+  // plugins: [new HtmlWebpackPlugin()],  
   resolve: {
     extensions: ['', '.js', '.json', '.scss', '.css']
-  }
+  },
 }
+
+
+
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+// var webpackConfig = {
+//   entry: 'index.js',
+//   output: {
+//     path: 'dist',
+//     filename: 'index_bundle.js'
+//   },
+//   plugins: [new HtmlWebpackPlugin()]
+// };
+
+  // plugins: [    
+  //   new HtmlWebpackPlugin ({
+  //     inject: true,
+  //     template: "index.html"
+  //   })
+  // ]
