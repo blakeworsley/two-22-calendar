d7fd7c1b76aa40dc6e171fb2781f12d0de7bcf28
Blake Worsley <blakeworsley@gmail.com>
2017-02-03 12:29:40 -0700

    adds ability to compress into mainbundle2

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,25 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     main: "./lib/index.js",
-    test: "mocha!./test/index.js"
   },
   output: {
     path: __dirname,
-    filename: "[name].bundle.js"
+    filename: "[name]2.bundle.js"
   },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
     ]
   },
-  // plugins: [new HtmlWebpackPlugin()],  
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject:true,
+    }),
+  ],  
   resolve: {
     extensions: ['', '.js', '.json', '.scss', '.css']
   },
 }
-
-
-
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-// var webpackConfig = {
-//   entry: 'index.js',
-//   output: {
-//     path: 'dist',
-//     filename: 'index_bundle.js'
-//   },
-//   plugins: [new HtmlWebpackPlugin()]
-// };
-
-  // plugins: [    
-  //   new HtmlWebpackPlugin ({
-  //     inject: true,
-  //     template: "index.html"
-  //   })
-  // ]
